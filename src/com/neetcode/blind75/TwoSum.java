2db5358a22f70907cc4688b7c609e046dd27255e
nirajkdas <neeraz141992@gmail.com>
2023-06-18 16:13:02 +0530

    two-sum

---
+++ src/com/neetcode/blind75/TwoSum.java
@@ -0,0 +1,29 @@
+package com.neetcode.blind75;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        /*
+        Make use of a hashmap,iterate through the array and for each element, deduct the element
+        from target and check if it exist in the map. if it exists, return the array indices
+        else add the element to hashmap with its index
+         */
+        HashMap<Integer, Integer> hs = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int remaining = target - nums[i];
+            if (hs.containsKey(remaining)) {
+                System.out.println(hs.get(remaining) + "," + i);
+                return;
+            } else
+                hs.put(nums[i], i);
+        }
+        System.out.println("-1");
+
+
+    }
+}
