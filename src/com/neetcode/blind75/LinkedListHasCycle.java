1a48e9c40ffd9694a6ca61e1bcd8c14ea454e9ad
nirajkdas <neeraz141992@gmail.com>
2023-06-19 21:30:39 +0530

    has-cycle

---
+++ src/com/neetcode/blind75/LinkedListHasCycle.java
@@ -0,0 +1,45 @@
+package com.neetcode.blind75;
+
+public class LinkedListHasCycle {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+
+        head.next = second;
+        second.next = third;
+        third.next = head;
+        System.out.println(hasCycle(head));
+    }
+
+    private static boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow)
+                return true;
+        }
+
+
+        return false;
+    }
+}
